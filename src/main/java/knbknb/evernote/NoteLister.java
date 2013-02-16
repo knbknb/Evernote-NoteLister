0fe5fe7b8c25d3b246dd41fa65710117dc9279b0
knbtest <knb@tnp-online.de>
2013-02-16 20:55:29 +0000

    Add date values to output

--- src/main/java/knbknb/evernote/NoteLister.java
+++ src/main/java/knbknb/evernote/NoteLister.java
@@ -13,6 +13,7 @@
  */
 package knbknb.evernote;
 
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.io.*;
 import java.security.MessageDigest;
@@ -229,11 +230,11 @@ public class NoteLister {
 
 			NoteList noteList = noteStore.findNotes(authToken, filter, 0, 1000);
 			List<Note> notes = noteList.getNotes();
-			for (Note note : notes) {
-				if (note.getTagGuidsSize() > 0) {
-					System.out.println(" * " + note.getTitle());
-				}
-			}
+//			for (Note note : notes) {
+//				if (note.getTagGuidsSize() > 0) {
+//					System.out.println(" * " + note.getTitle());
+//				}
+//			}
 
 			this.cnt = 0;
 			NoteCollectionCounts nbcnt = noteStore.findNoteCounts(authToken,
@@ -241,17 +242,36 @@ public class NoteLister {
 			Map<String, Integer> m = nbcnt.getNotebookCounts();
 			int noteCount = m.get(nbook.getGuid());
 			System.out.println(" -- " + noteCount + " -- ");
+			SimpleDateFormat ISO8601DATEFORMAT = new SimpleDateFormat("yyyy-MM-dd' 'HH:mm", Locale.GERMANY);
+			
 			for (int i = 0; i < noteCount; i += 50) {
 
 				noteList = noteStore.findNotes(authToken, filter, i, i + 50);
 				notes = noteList.getNotes();
+				System.out.println(nbook.getName() + " - Notes with tagcount == 0");
 				for (Note note : notes) {
 					this.cnt++;
-					int tgcnt = note.getTagNamesSize();
+					int tgcnt = note.getTagGuidsSize();
+					Date crd = new Date(note.getCreated());
 					if (tgcnt == 0) {
-						System.out.println(" " + cnt + " " + note.getTitle()
+						
+						System.out.println("#" + cnt + ":  " + ISO8601DATEFORMAT.format(crd) + "   " + note.getTitle()
 								+ " - tags: " + tgcnt);
 					}
+		            
+				}
+				System.out.println("");
+				System.out.println(nbook.getName() + " - Notes with tagcount > 0");
+				for (Note note : notes) {
+					this.cnt++;
+					int tgcnt = note.getTagGuidsSize();
+					Date crd = new Date(note.getCreated());
+					if (tgcnt != 0) {
+						
+						System.out.println(" " + cnt + ":  " + ISO8601DATEFORMAT.format(crd) + "   " + note.getTitle()
+								+ " - tags: " + tgcnt);
+					}
+		            
 				}
 			}
 
@@ -276,74 +296,6 @@ public class NoteLister {
 		return token;
 	}
 
-	/**
-	 * Helper method to read the contents of a file on disk and create a new
-	 * Data object.
-	 */
-	private static Data readFileAsData(String fileName) throws Exception {
-
-		// Read the full binary contents of the file
-		FileInputStream in = new FileInputStream(fileName);
-		ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
-		byte[] block = new byte[10240];
-		int len;
-		while ((len = in.read(block)) >= 0) {
-			byteOut.write(block, 0, len);
-		}
-		in.close();
-		byte[] body = byteOut.toByteArray();
-
-		// Create a new Data object to contain the file contents
-		Data data = new Data();
-		data.setSize(body.length);
-		data.setBodyHash(MessageDigest.getInstance("MD5").digest(body));
-		data.setBody(body);
-
-		return data;
-	}
-
-	/**
-	 * Helper method to convert a byte array to a hexadecimal string.
-	 */
-	public static String bytesToHex(byte[] bytes) {
-		StringBuilder sb = new StringBuilder();
-		for (byte hashByte : bytes) {
-			int intVal = 0xff & hashByte;
-			if (intVal < 0x10) {
-				sb.append('0');
-			}
-			sb.append(Integer.toHexString(intVal));
-		}
-		return sb.toString();
-	}
-
-	public Map readProperties() {
-		Properties properties = System.getProperties();
-
-		@SuppressWarnings("unchecked")
-		Enumeration<String> enumeration = (Enumeration<String>) properties
-				.propertyNames();
-		@SuppressWarnings("rawtypes")
-		Map propmap = new HashMap();
-		String token = "nothing found";
-		String notebookname = "default_notebook";
-
-		while (enumeration.hasMoreElements()) {
-			String var = (String) enumeration.nextElement();
-			String val = properties.getProperty(var);
-			if (var.equalsIgnoreCase("developertoken")) {
-				token = val;
-			}
-
-			if (var.equalsIgnoreCase("notebookname")) {
-				notebookname = val;
-			}
-		}
-
-		propmap.put("token", token);
-		propmap.put("notebookname", notebookname);
-		return propmap;
-	}
 
 	public static String getNotebook() {
 		return notebook;
